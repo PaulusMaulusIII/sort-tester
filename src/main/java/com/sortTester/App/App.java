07393f0fdaef439f53e75b842afe2b6ad5dadaa8
Paul Schröder <paulschroeder05@outlook.de>
2024-02-08 23:07:59 +0100

    Removed unnecessary imports

--- src/main/java/com/sortTester/App/App.java
+++ src/main/java/com/sortTester/App/App.java
@@ -7,8 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
